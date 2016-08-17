fb3245b4cc0f6ccd3452b10c9c05693d1652bd5b
Aimen Khakwani and Katy Henning <aimen@>
2016-08-17 10:21:44 -0700

    add code to attempt to notify user if they enter an empty form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // Business logic
 var leapYear = function (year){
+  console.log(year);
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
-  } else if (year === ""){
-    alert("Please enter a year.");
-  } else {
+  } //else if (year.length === 0){
+  //   alert("Please enter a year.");
+  // }
+   else {
     return false;
   }
 }
@@ -12,8 +14,18 @@ var leapYear = function (year){
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    if (year !== 0 1 2 3 4 5 6 7 8 9) {
+      alert("Please enter a year.")
+    }
+    //check if length is 0 here
+
+    //if length is 0, show alert,
+
+    //otherwise parse, and call function.
+    else{
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
+  }
   });
 });
